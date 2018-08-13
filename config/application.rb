25c412186ccfa7803ed21ef39bd771e73e8fc0ba
mp18red <mp18red@gmail.com>
2018-08-13 10:39:52 -0400

    time zone

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,12 @@ Bundler.require(*Rails.groups)
 
 module JroxEnterprises
   class Application < Rails::Application
+    config.Time_zone = "Eastern Time (US & Canada)"
+ config.active_record.default_timezone = :utc
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.time_zone = "Eastern Time (US & Canada)"
- config.active_record.default_timezone = :local
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
