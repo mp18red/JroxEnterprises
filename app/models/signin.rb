2ee6a69a0f9d8af765bb82c489ce6d172c0a13cd
mp18red <mp18red@gmail.com>
2018-08-25 15:29:47 -0400

    Search functionality added + button for bbs and videos

--- app/models/signin.rb
+++ app/models/signin.rb
@@ -1,3 +1,9 @@
 class Signin < ApplicationRecord
-    
+    def self.search(search)
+        if search
+            where(["created_at LIKE ?", "%#{search}%"])
+            else
+              Signin.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+            end
+end
 end
